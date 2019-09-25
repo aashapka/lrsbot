87abfe6ce314069d718022d79051a4cd0c10005f
aashapka <54244790+aashapka@users.noreply.github.com>
2019-09-25 21:30:50 +0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ while True:
     if res == 'PING :tmi.twitch.tv\r\n':
         s.send(('PONG :tmi.twitch.tv\r\n').encode('utf-8'))
     else:
-       try:
+        try:
             name = re.search(r"\w+", res).group(0)
             msg = chat_msg.sub('', res)
             print(res)
@@ -49,26 +49,4 @@ while True:
             if (msg.strip() == "!правила"):
                 s.send('PRIVMSG #{} : Первое Правило чата Марадона 1-марадон всегда лох . Второе правило чата марадона 2-если я назвал тебя куколдом то я долбаеб\r\n'.format(config.CHAN, name).encode('utf-8'))
         except:
-            None
-
-        #i = 0
-        #for x in msg.strip():
-        #    if x.isupper():
-        #        i+=1
-        #percent = i/len(msg.strip())
-        #print(percent)
-        #if (percent > 0.49) and (msg.strip() not in smiles):
-           # utils.timeout(s, name) 
-        
-        
-        
-        #if msg.strip() == 'grav+' and (name in mods):
-        #    utils.rip_plus(s)
-        #elif msg.strip() == 'grav-' and (name in mods):
-        #    utils.rip_minus(s)
-        #elif msg.strip() == '!rip':
-        #    utils.rip_print(s, name)
-        #elif msg.strip() == '!pasha' and (name in sosat):
-         #   utils.pxs(s, name)
-        #elif msg.strip().startswith('!хуй') == True:
-        #    utils.fuck(s, name, msg)
+            print('shit')
